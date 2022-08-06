4509a0b17ce96796d4cd926959546e4478b507d4
Leonardo <terlizzileonardo@gmail.com>
2022-08-05 22:54:54 -0300

    working on navbar pre dynamic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,253 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-navbar></the-navbar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import TheNavbar from "./components/layout/TheNavbar.vue";
+  export default {
+    name: "App",
+    components: { TheNavbar },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
+
+  .animLeft {
+    animation: leftTransition 0.5s ease none;
+    animation-delay: 500ms;
+  }
+  .animLeft2 {
+    animation: leftTransition 0.8s ease none;
+    animation-delay: 500ms;
+  }
+
+  .animLeft3 {
+    animation: leftTransition 1s ease none;
+    animation-delay: 500ms;
+  }
+
+  .animRight {
+    animation: rightTransition 1.2s ease none;
+    animation-delay: 500ms;
+  }
+
+  .fade {
+    animation: fade 0.5s ease none;
+  }
+
+  .slowFade {
+    animation: fade 1.5s ease none;
+  }
+
+  .scale:hover {
+    animation: scale 0.7s ease none;
+  }
+
+  .blop:hover {
+    animation: blop 0.5s ease none;
+  }
+
+  .big:hover {
+    animation: big 0.5s ease-in-out none;
+  }
+
+  .biger:hover {
+    animation: biger 0.5s ease-in-out none;
+  }
+
+  .moveR:hover {
+    animation: moverR 1s ease-in-out none;
+  }
+
+  .bigBlop:hover {
+    animation: bigBlop 1s ease-in-out none;
+  }
+
+  .moverDiag:hover {
+    animation: moverDiag 1s ease none;
+  }
+
+  .moverDiagBrut:hover {
+    animation: moverDiagBrut 1s ease-in-out none;
+  }
+
+  @keyframes leftTransition {
+    from {
+      transform: translateX(-2000px);
+    }
+    to {
+      transform: translateX(0px);
+    }
+  }
+
+  @keyframes rightTransition {
+    from {
+      transform: translateX(5000px);
+    }
+    to {
+      transform: translateX(0px);
+    }
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes scale {
+    from {
+      transform: none;
+    }
+    25% {
+      transform: rotate(10deg);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+
+    75% {
+      transform: rotate(-10deg);
+    }
+
+    95% {
+      transform: scale(0.9);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes blop {
+    from {
+      transform: none;
+    }
+    25% {
+      transform: rotate(25deg);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+
+    60% {
+      transform: rotate(-35deg);
+    }
+
+    95% {
+      transform: scale(0.9);
+    }
+
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes bigBlop {
+    from {
+      transform: none;
+    }
+    25% {
+      transform: rotate(45deg);
+    }
+    50% {
+      transform: scale(3);
+      border-radius: 50%;
+      font-size: 20rem;
+    }
+
+    60% {
+      transform: rotate(-45deg);
+    }
+
+    95% {
+      transform: scale(0.9);
+    }
+
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes big {
+    from {
+      transform: none;
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes biger {
+    from {
+      transform: none;
+    }
+    50% {
+      transform: scale(3);
+      border-radius: 50%;
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes moverR {
+    from {
+      transform: none;
+    }
+    50% {
+      transform: translateX(10rem);
+      border-radius: 70%;
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes moverDiag {
+    from {
+      transform: scale(1.1);
+    }
+    25% {
+      transform: translate(-12rem, -6rem);
+    }
+    50% {
+      transform: scale(3);
+      border-radius: 35%;
+    }
+    90% {
+      transform: translate(0, 0);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes moverDiagBrut {
+    from {
+      transform: scale(1);
+    }
+    25% {
+      transform: translateX(15rem);
+    }
+    50% {
+      transform: translate(8rem, -10rem);
+      border-radius: 5%;
+    }
+    90% {
+      transform: translate(0, 0);
+    }
+    to {
+      transform: scale(1);
+      transform: translate3d();
+    }
+  }
 </style>
---
+++ src/components/layout/TheNavbar.vue
@@ -0,0 +1,225 @@
+<template>
+  <nav class="menu slowFade">
+    <div class="menu__header">
+      <a href="#home" title="Home">
+        <i class="fa-solid fa-house menu__header-icon scale" tooltip="asda"></i
+      ></a>
+
+      <h5 class="menu__header-title">
+        const <span class="punto">ltizzi </span> = new
+        <span style="color: #8be9fd">Ltizzi</span><span class="punto">(</span>
+        <span style="color: #f1fa8c">"Leonardo Terlizzi"</span>
+        <span class="punto">);</span>
+      </h5>
+    </div>
+    <nav class="menu__nav">
+      <ul class="menu__ul">
+        <li class="menu__item scale">
+          <a
+            href="#about"
+            class="menu__link"
+            style="color: #bd93f9"
+            title="Acerca de mí"
+            >ltizzi</a
+          >
+        </li>
+        <p class="punto">.get<span style="color: #8be9fd">(</span></p>
+        <li class="menu__item scale">
+          <a
+            href="#skills"
+            class="menu__link"
+            style="color: #f1fa8c"
+            title="ver habilidades"
+            >Skills<span class="punto">,</span></a
+          >
+        </li>
+        <li class="menu__item scale">
+          <a
+            href="#hobbies"
+            class="menu__link"
+            style="color: #50fa7b"
+            title="ver hobbies"
+            >Hobbies</a
+          >
+        </li>
+
+        <p class="punto"><span style="color: #8be9fd">)</span>.</p>
+        <li class="menu__item scale">
+          <a
+            href="#formacion"
+            class="menu__link"
+            style="color: #8be9fd"
+            title="ver formación"
+            >getFormación()</a
+          >
+        </li>
+        <p class="punto">.</p>
+        <li class="menu__item scale">
+          <a
+            href="#proyectos"
+            class="menu__link"
+            style="color: #ff79c6"
+            title=""
+            >getProyectos()</a
+          >
+        </li>
+        <p class="punto">.</p>
+        <li class="menu__item scale">
+          <a
+            href="#contacto"
+            class="menu__link"
+            style="color: #ffb86c"
+            title="mandar mensaje"
+            >Contacto() <span class="punto">;</span></a
+          >
+        </li>
+      </ul>
+    </nav>
+  </nav>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        menuItems: [
+          {
+            text: "ltizzi",
+            style: "color: #bd93f9",
+            link: "#about",
+            title: "Acerca de mí",
+            puntuación: [
+              {
+                tipo: ".get",
+                style: "color: #fff",
+              },
+              { tipo: "(", style: "color: #8be9fd" },
+            ],
+          },
+          {
+            text: "Skills",
+            style: "color: #f1fa8c",
+            link: "#skills",
+            title: "ver habilidades",
+            puntuación: [{ tipo: ",", style: "color:  #fff" }],
+          },
+          {
+            text: "Hobbies",
+            style: "color: #50fa7b",
+            link: "#hobbies",
+            title: "ver hobbies",
+            puntuación: [
+              { tipo: ")", style: "color: #8be9fd" },
+              { tipo: ".", style: "color: #fff" },
+            ],
+          },
+          {
+            text: "getFormación()",
+            style: "color: #8be9fd",
+            link: "#formacion",
+            title: "ver formación",
+            puntuación: [{ tipo: ".", style: "color:  #fff" }],
+          },
+          {
+            text: "getProyectos()",
+            style: "color: #ff79c6",
+            link: "#proyectos",
+            title: "ver proyectos",
+            puntuación: [{ tipo: ".", style: "color:  #fff" }],
+          },
+          {
+            text: "Contacto()",
+            style: "color: #ffb86c",
+            link: "#contacto",
+            title: "mandar mensaje",
+            puntuación: [{ tipo: ";", style: "color:#fff" }],
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  html {
+    scroll-behavior: smooth;
+  }
+
+  .menu {
+    display: flex;
+    /* background-color: rgba(255, 251, 251, 0.9); */
+    background: #282a36;
+    opacity: 0.9;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    height: 2.5rem;
+    position: fixed;
+    z-index: 10;
+  }
+
+  .menu__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    text-align: center;
+  }
+
+  .menu__header-title {
+    color: #ff79c6;
+    font-weight: 700;
+    font-family: "droid-sans-mono", monospace;
+    font-size: 1.125rem;
+    padding-left: 0.5rem;
+    margin: 0;
+  }
+
+  .menu__header-icon {
+    color: #50fa7b;
+    font-size: 1.2rem;
+    margin-left: 1rem;
+  }
+
+  .menu__ul {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    margin: 0;
+  }
+
+  .punto {
+    color: #f8f8f2;
+    font-weight: 700;
+    font-family: "droid-sans-mono", monospace;
+    margin: 0;
+  }
+
+  .menu__link {
+    text-decoration: none;
+    padding-right: 0.5rem;
+    font-family: "M PLUS Rounded 1c", sans-serif;
+    font-size: 1.125rem;
+    font-weight: 700;
+  }
+
+  .menu__item {
+    margin: 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    /* .menu__ul {
+    display: none;
+  } */
+
+    .menu__header-title {
+      font-size: 0.5rem;
+    }
+  }
+  @media screen and (max-width: 1280px) {
+    .menu__header-title {
+      font-size: 1rem;
+    }
+    .menu__ul {
+      display: none;
+    }
+  }
+</style>
